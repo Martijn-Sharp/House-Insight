1714bbcb40671bc355b41535841c3f238d796aec
Martijn <citybiker@live.nl>
2016-12-04 14:12:37 +0100

    Made the test project able to run tests and added a dummy test

---
+++ test/Martijn.HouseInsight.EntityFramework.Tests/DummyTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Martijn.HouseInsight.EntityFramework.Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
